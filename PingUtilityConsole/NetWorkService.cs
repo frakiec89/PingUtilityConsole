8887599b23fe7b398a3b7b193694e0d19fbdfaee
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-03-07 10:51:12 +0400

    Добавьте файлы проекта.

---
+++ PingUtilityConsole/NetWorkService.cs
@@ -0,0 +1,60 @@
+﻿// nocopy
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+internal class NetWorkService
+{
+
+    internal static void Ping(string adress)
+    {
+        Ping pingSender = new Ping();
+        PingOptions options = new PingOptions();
+
+        // Используем значение Ttl по умолчанию, равное 128,
+        
+        // поведение фрагментации.
+        options.DontFragment = true;
+
+        // Создаем буфер из 32 байт данных для передачи.
+        string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        byte[] buffer = Encoding.ASCII.GetBytes(data);
+       
+        int timeout = 120;
+
+        PingReply reply = pingSender.Send(adress ,  timeout, buffer, options);
+       
+        if (reply.Status == IPStatus.Success)
+        {
+          
+            if (reply.RoundtripTime == 0)
+                 Console.WriteLine($"Время запроса: <1мс");
+            else
+                Console.WriteLine($"Время запроса: {reply.RoundtripTime}" );
+
+            Console.WriteLine($"Адресс: {reply.Address}");
+
+            Console.WriteLine($"TTL: {reply.Options.Ttl}");
+            Console.WriteLine($"Потерянные пакеты: {reply.Options.DontFragment}" );
+            Console.WriteLine($"Размер фрагмента: {reply.Buffer.Length}" );
+        }
+        else
+        {
+            Console.WriteLine("ресурс не ответил");
+        }
+    }
+
+
+
+    internal static  void GetDNS (string domen)
+    {
+        var googleEntry =  Dns.GetHostEntry(domen);
+
+        Console.WriteLine($"На домене {domen} найдены следующие адрреса:");
+        foreach (var ip in googleEntry.AddressList)
+        {
+            Console.WriteLine(ip);
+        }
+    }
+}
---
+++ PingUtilityConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿// nocopy
+using System.Net;
+
+// приветствие 
+Console.WriteLine("Привет  я  программа  которая работает  с  сетью");
+// меню
+
+while (true)
+{
+    PrintComand();
+
+    // заготовка на  будущее - вдруг  будет  много  инструментов 
+
+    switch (Console.ReadLine().ToLower())
+    {
+        case "ping": PingCommand(); break;  // пока что  только  одна команда 
+        case "dns": DNSCommand(); break;  // пока что  только  одна команда 
+        default: Console.WriteLine("Команда не распоздана, попробуйте  еще раз");
+            break;
+    }
+
+}
+
+
+
+// список команд
+void PrintComand()
+{
+    Console.WriteLine("Для пинга ресурса ввидите команду \"ping\""); // возможны  расширения команд
+
+    Console.WriteLine("что бы  получить  ип  адресса хоста введите команду  \"dns\""); // возможны  расширения команд
+}
+
+
+//
+void PingCommand()
+{
+    Console.WriteLine("Введите адресс ресурса:");
+    string adress  = Console.ReadLine();
+
+    for (int i = 0; i < 5; i++) // будет  5 попыток 
+    {
+        try
+        {
+     // !!этот класс придумал  я   в нем метод
+            NetWorkService.        Ping(adress); // вынисим  логику  в  отдельный класс 
+        }
+        catch (Exception ex) // если ошибка
+        {
+            Console.WriteLine($"eror {ex.Message}");
+            return; //выйти  из  метода 
+        }
+        finally //  в любом случае выдержать паузу 
+        {
+            Console.WriteLine("____________");
+            Thread.Sleep(1000); // пауза
+        }
+    }
+
+}
+
+
+void DNSCommand()
+{
+    Console.WriteLine("Введите доменное  имя:");
+    string adress = Console.ReadLine();
+    try
+    {
+        // !!этот класс придумал  я   в нем метод
+        NetWorkService.GetDNS(adress); // вынисим  логику  в  отдельный класс 
+    }
+    catch (Exception ex) // если ошибка
+    {
+        Console.WriteLine($"eror {ex.Message}");
+        return; //выйти  из  метода 
+    }
+    finally //  в любом случае выдержать паузу 
+    {
+        Console.WriteLine("____________");
+        Thread.Sleep(1000); // пауза
+    }
+
+}
+
